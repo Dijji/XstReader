c2f8154f8b9fcd95788debf86e6b6dd13f0ca61a
Dijji <27084934+Dijji@users.noreply.github.com>
2020-04-07 10:29:30 +0100

    Add additional exception catching for an unhandled exception occurring in version 1.8

--- App.xaml.cs
+++ App.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Windows;
 
 namespace XstReader
@@ -14,10 +15,26 @@ namespace XstReader
     {
         private void Application_Startup(object sender, StartupEventArgs e)
         {
-            MainWindow wnd = new MainWindow();
+            AppDomain currentDomain = AppDomain.CurrentDomain;
+            currentDomain.UnhandledException += CurrentDomain_UnhandledException;
+            TaskScheduler.UnobservedTaskException += TaskScheduler_UnobservedTaskException;
+
+                MainWindow wnd = new MainWindow();
             if (e.Args.Length == 1)
                 wnd.OpenFile(e.Args[0]);
             wnd.Show();
+        }
+
+        private void TaskScheduler_UnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs e)
+        {
+            Exception ex = (Exception)e.Exception;
+            MessageBox.Show(ex.Message, "Unhandled thread exception");
+        }
+
+        private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Exception ex = (Exception)e.ExceptionObject;
+            MessageBox.Show(ex.Message, "Unhandled exception");
         }
     }
 }
