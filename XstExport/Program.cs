8bf45dcdc47f1b385a0724157a87614047816fdd
Dijji <27084934+Dijji@users.noreply.github.com>
2020-12-31 12:26:02 +0000

    A couple more fixes to testing issues

--- XstExport/Program.cs
+++ XstExport/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using NDesk.Options;
 using XstReader;
 
@@ -212,8 +213,12 @@ namespace XstExport
                 return;
             }
 
+            bool createdByUs = false;
             if (!Directory.Exists(exportDir))
+            {
                 Directory.CreateDirectory(exportDir);
+                createdByUs = true;
+            }
 
             switch (command)
             {
@@ -230,6 +235,15 @@ namespace XstExport
                 default:
                     break;
             }
+
+            // If we create the directory, clean it up if nothing is put in it
+            if (createdByUs)
+            {
+                var di = new DirectoryInfo(exportDir);
+                if (!di.EnumerateFiles().Any() &&
+                    !di.EnumerateDirectories().Any())
+                    di.Delete();
+            }
         }
 
         private static Folder FindOutlookFolder(Folder root, string outlookFolder)
@@ -243,8 +257,9 @@ namespace XstExport
             while (q.Count > 0)
             {
                 var f = q.Dequeue();
-                if (FolderMatches(f, folders))
-                    return f;
+                var match = FolderMatch(f, folders);
+                if (match != null)
+                    return match;
 
                 foreach (var child in f.Folders)
                     q.Enqueue(child);
@@ -253,23 +268,23 @@ namespace XstExport
             return null;
         }
 
-        private static bool FolderMatches(Folder folder, string[] folderNames)
+        private static Folder FolderMatch(Folder folder, string[] folderNames)
         {
             // First name segment must match
             if (String.Compare(folder.Name, folderNames[0], true) != 0)
-                return false;
+                return null;
 
             // And subsequent segments must be matched by some child
             for (int i = 1; i < folderNames.Length; i++)
             {
                 // This exploits the fact that there can be only one child of a given folder with a given name
-                folder = folder.Folders.Find(f => String.Compare(f.Name, folderNames[i], true) != 0);
+                folder = folder.Folders.Find(f => String.Compare(f.Name, folderNames[i], true) == 0);
                 if (folder == null)
-                    return false;
+                    return null;
             }
 
-            // All segments of the name have been matched
-            return true;
+            // All segments of the name have been matched - return the innermost
+            return folder;
         }
 
         private static string CreateDirectoryIfNeeded(string rootDirName, string dirName)
@@ -374,11 +389,12 @@ namespace XstExport
                             {
                                 actionName = "CreateNewer";
                             } 
-                            else { 
+                            else
+                            { 
                                 actionName = "Skip";
                             }
                         }
-                        Console.WriteLine(String.Format("{0} : {1}" , actionName, attachmentExpectedName));
+                        Console.WriteLine($"{actionName} : {attachmentExpectedName}");
                         switch (actionName)
                         {
                             case "Create":
