72fe6c85e45c84bef2a7d4084c439afacfb1a2dd
Dijji <27084934+Dijji@users.noreply.github.com>
2021-01-01 12:14:46 +0000

    Package Core as part of the two executables, to retain single file execution. The DLL is maintained as part of the solution.

--- XstReader.Core/Extensions.cs
+++ XstReader.Core/Extensions.cs
@@ -1,7 +1,10 @@
 ﻿// Copyright (c) 2020, Dijji, and released under Ms-PL.  This can be found in the root of this distribution. 
 
 using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace XstReader
@@ -21,6 +24,17 @@ namespace XstReader
                 removeInvalidChars = new Regex(String.Format("[{0}]", Regex.Escape(new string(Path.GetInvalidFileNameChars()))),
                         RegexOptions.Singleline | RegexOptions.Compiled | RegexOptions.CultureInvariant);
             return removeInvalidChars.Replace(value, with);
-        }      
+        }
+
+        public static void PopulateWith<T>(this ObservableCollection<T> collection, List<T> list)
+        {
+            collection.Clear();
+            foreach (T value in list)
+                collection.Add(value);
+        }
+
+        public static IEnumerable<T> Flatten<T>(this IEnumerable<T> e, Func<T, IEnumerable<T>> f)
+          => e.SelectMany(c => f(c).Flatten(f)).Concat(e);
+
     }
 }
